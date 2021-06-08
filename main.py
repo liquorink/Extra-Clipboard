94f452d645c6eb640a15f5c2b840476e2eaf30e9
Idan Kazom <50081971+liquorink@users.noreply.github.com>
2021-06-08 20:06:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import win32clipboard 
-import time #required to ask whether the content is changing or has changed & disable the script whenever you want.
-
+import win32clipboard
+import time  # required to ask whether the content is changing or has changed & disable the script whenever you want.
+import threading
 # Checking if the new data is identical to the data that has been called before.
 old = ""
 
@@ -9,6 +9,7 @@ while True:
     data = win32clipboard.GetClipboardData()
     win32clipboard.CloseClipboard()
     if old != data:
-        with open("cclip_history.txt", 'a+') as f:
+        with open("clip_history.txt", "a+") as f:
             f.write(data + "\n")
         old = data
+    time.sleep(0.5)
